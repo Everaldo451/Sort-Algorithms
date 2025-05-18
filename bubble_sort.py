c772784eeed42059943c42bf8215125883d620e4
Everaldo451 <everaldo.work.jr@gmail.com>
2025-05-18 15:25:54 -0300

    bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,17 @@
+class BubbleSort:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def sort(cls, lst:list):
+        for i in range(len(lst)-1, 0, -1):
+            for j in range(i):
+                if lst[j+1]<lst[j]:
+                    lower = lst[j+1]
+                    lst[j+1] = lst[j]
+                    lst[j] = lower
+        return lst
+
+
+lst = [10,3,2,7,6,4,1]
+print(BubbleSort.sort(lst))
